63e779dac4995cae3add86068e8ec661182ea910
RassaLibre <T-Prochazka@seznam.cz>
2019-02-24 15:45:26 +0100

    fixing tests

--- app/state/players/__tests__/actions.test.js
+++ app/state/players/__tests__/actions.test.js
@@ -1,5 +1,6 @@
 import {
-  addCoinsToPlayer
+  addCoinsToPlayer,
+  removeCoinsFromPlayer,
 } from '../actions'
 
 import RootReducer from '../../../RootReducer'
@@ -7,19 +8,28 @@ import RootReducer from '../../../RootReducer'
 const getPlayerById = ({ players }, playerId) =>
   players.find(p => p.id === playerId)
 
-describe("Players actions", ()=>{
+describe("Players actions", () => {
 
   let store
 
-  beforeEach(()=> store = RootReducer)
+  beforeEach(() => store = RootReducer)
 
-  it('Should add coints to the player', ()=>{
+  it('Should add coins to the player', () => {
     const playerId = 1
     const coinsToBeAdded = 2
     const initPlayer = getPlayerById(store.getState(), playerId)
-    addCoinsToPlayer(playerId, coinsToBeAdded)(store.dispatch)
+    store.dispatch(addCoinsToPlayer(playerId, coinsToBeAdded))
     const updatedPlayer = getPlayerById(store.getState(), playerId)
     expect(initPlayer.coins + coinsToBeAdded).toBe(updatedPlayer.coins)
+  })
+
+  it('Should remove coins from the player', () => {
+    const playerId = 1
+    const coinsToBeRemoved = 2
+    const initPlayer = getPlayerById(store.getState(), playerId)
+    store.dispatch(removeCoinsFromPlayer(playerId, coinsToBeRemoved))
+    const updatedPlayer = getPlayerById(store.getState(), playerId)
+    expect(initPlayer.coins - coinsToBeRemoved).toBe(updatedPlayer.coins)
   })
 
 })
