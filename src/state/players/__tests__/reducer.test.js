5c7af2f8819f241135f94d82bc7b4f007c2f3c19
RassaLibre <T-Prochazka@seznam.cz>
2019-03-15 18:39:30 +0100

    adding tests for removing an d adding cards to a player

--- src/state/players/__tests__/reducer.test.js
+++ src/state/players/__tests__/reducer.test.js
@@ -1,7 +1,9 @@
 import Reducer from '../reducer'
 import {
   ADD_COINS_TO_PLAYER,
-  REMOVE_COINS_FROM_PLAYER
+  REMOVE_COINS_FROM_PLAYER,
+  ADD_CARD_TO_PLAYER,
+  REMOVE_CARD_FROM_PLAYER,
 } from '../consts'
 
 
@@ -35,6 +37,32 @@ describe("Players reducer", ()=>{
     })
     expect(newState.find(p => p.id === playerId).coins + numOfCoins)
       .toBe(initState.find(p => p.id === playerId).coins)
+  })
+
+  it("Should add a card to a player", () => {
+    const playerId = 1,
+      card = { id: 34 }
+    const newState = reducer(initState, { type: ADD_CARD_TO_PLAYER, playerId, card })
+    expect(newState.find(p => p.id === playerId).cards.find(c => c.id === card.id))
+      .toBe(card)
+  })
+
+  describe("removing cards from a player" , () => {
+
+    const CARD_TO_BE_REMOVED = { id: 44 }, playerId = 1
+    let defaultState = null
+
+    // make sure that the player has a card before we attempt to remove it
+    beforeEach(() => {
+      defaultState = reducer(initState, { type: ADD_CARD_TO_PLAYER, playerId, card: CARD_TO_BE_REMOVED })
+    })
+
+    it("Should remove a card from a player", () => {
+      const newState = reducer(defaultState, { type: REMOVE_CARD_FROM_PLAYER, playerId, card: CARD_TO_BE_REMOVED })
+      expect(newState.find(p => p.id === playerId).cards.find(c => c.id === CARD_TO_BE_REMOVED.id))
+        .toBeUndefined()
+    })
+
   })
 
 })
