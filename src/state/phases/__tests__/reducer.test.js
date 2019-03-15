14409fa3fc91fae19e5fbcf86290d8df3169008e
RassaLibre <T-Prochazka@seznam.cz>
2019-03-15 18:45:40 +0100

    Adding tests for `playedCards` in trade phase

--- src/state/phases/__tests__/reducer.test.js
+++ src/state/phases/__tests__/reducer.test.js
@@ -1,6 +1,8 @@
 import Reducer from '../reducer'
 import {
-  TOGGLE_PHASE
+  TOGGLE_PHASE,
+  INCREMENT_ACTIVE_CARDS_PLAYED,
+  SET_CARDS_PLAYED_IN_TRADE_PHASE
 } from '../consts'
 
 describe("Phase reducer", ()=>{
@@ -17,6 +19,16 @@ describe("Phase reducer", ()=>{
     expect(initState.activePhase).not.toBe(newState.activePhase)
     const newNewState = reducer(newState, {type: TOGGLE_PHASE})
     expect(newNewState.activePhase).toBe(initState.activePhase)
+  })
+
+  it("should increment the number of played cards", () => {
+    const newState = reducer(initState, { type: INCREMENT_ACTIVE_CARDS_PLAYED })
+    expect(newState.tradePhase.cardsPlayed).toBe(initState.tradePhase.cardsPlayed + 1)
+  })
+
+  it("should set the number of played cards", () => {
+    const newState = reducer(initState, { type: SET_CARDS_PLAYED_IN_TRADE_PHASE, value: 33 })
+    expect(newState.tradePhase.cardsPlayed).toBe(33)
   })
 
 })
